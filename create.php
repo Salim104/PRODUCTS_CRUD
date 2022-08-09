211f37cbb78db9d8fbce70ca139f76d3b2e28403
salim104 <salimshaban885@gmail.com>
2022-08-09 17:56:19 +0200

    PRODUCT_CRUD

---
+++ create.php
@@ -0,0 +1,121 @@
+
+<?php
+
+// require_once "functions.php";
+
+  $pdo = new PDO("mysql:host=localhost;port=3306;dbname=products","root","");
+  $pdo ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+$errors = [];
+
+$title = '';
+$description = '';
+$price = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+
+    $image = $_FILES['image'] ?? null;
+    $imagePath = '';
+
+    if (!is_dir('images')) {
+        mkdir('images');
+    }
+
+    if ($image && $image['tmp_name']) {
+        $imagePath = 'images/' . randomString(8) . '/' . $image['name'];
+        mkdir(dirname($imagePath));
+        move_uploaded_file($image['tmp_name'], $imagePath);
+    }
+
+    if (!$title) {
+        $errors[] = 'Product title is required';
+    }
+
+    if (!$price) {
+        $errors[] = 'Product price is required';
+    }
+
+    if (empty($errors)) {
+        $statement = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
+                VALUES (:title, :image, :description, :price, :date)");
+        $statement->bindValue(':title', $title);
+        $statement->bindValue(':image', $imagePath);
+        $statement->bindValue(':description', $description);
+        $statement->bindValue(':price', $price);
+        $statement->bindValue(':date', date('Y-m-d H:i:s'));
+
+        $statement->execute();
+        header('Location: index.php');
+    }
+
+}
+
+function randomString($n)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $str = '';
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $str .= $characters[$index];
+    }
+
+    return $str;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+     <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <style>
+      body{
+        padding: 50px;
+      }
+    </style>
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Create New Products</h1>
+
+ <?php if(!empty($errors)) { ?>
+
+     <div class="alert alert-danger">
+        <?php foreach ($errors as $error){ ?>
+                <div><?php echo $error ?></div>
+       <?php } ?>
+    </div>
+
+  <?php }?>
+
+  <form action="" method="post" enctype="multipart/form-data">
+      <div class="form-group mb-3">
+        <label>Product Image</label>
+        <br>
+        <input type="file" name="image">
+      </div>
+      <div class="form-group mb-3">
+        <label>Product Title</label>
+        <input type="text" class="form-control"  name="title" value="<?php echo $title ?>">
+      </div>
+      <div class="form-group mb-3">
+        <label>Product Description</label>
+        <textarea class="form-control"  name="description" value="<?php echo $description ?>"></textarea>
+      </div>
+      <div class="form-group mb-3">
+        <label>Product Price</label>
+        <input type="number" step=".01"  name="price" class="form-control" value="<?php echo $price ?>">
+      </div>
+       <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+  
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,105 @@
+<?php
+// the connection to database
+   $pdo = new PDO("mysql:host=localhost;port=3306;dbname=products","root","");
+   $pdo ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+ 
+   $search = $_GET['search'] ?? '';
+
+   if($search){
+       $statement = $pdo->prepare('SELECT * FROM products WHERE title LIKE :title ORDER BY create_date DESC');
+
+       $statement->bindValue(':title',"%$search%");
+      
+   }else{
+     // selecting the list in the table
+   $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+   }
+
+
+   $statement->execute();
+   $products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+ 
+
+
+?>
+
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <style>
+      body{
+        padding: 50px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Products CRUD</h1>
+    <p>
+      <a href="create.php" class="btn btn-sm btn-success">Create</a>
+    </p>
+
+      <form>
+              <div class="input-group " style="width: 20rem;">
+                 <input type="text" class="form-control" placeholder="Search for products" name="search">
+                 <button class="btn btn-outline-secondary" type="submit">Search</button>
+              </div>
+      </form>
+
+   <table class="table">
+  <thead>
+    <tr>
+     <th>#</th>
+        <th>image</th>
+        <th>Title</th>
+
+        <th>price</th>
+        <th>create_date</th>
+        <th>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+     <?php foreach ($products as $i =>  $product){?>
+            <tr>
+                <th><?php echo $i + 1 ?></th>
+                <td>
+                  <img src="<?php echo $product['image']?>" alt="" style="width: 4rem;">
+                </td>
+                <td><?php echo $product['title'] ?></td>
+                <td><?php echo $product['price'] ?></td>
+                <td><?php echo $product['create_date'] ?></td>
+                <td>
+                       <a href="update.php?id=<?php echo $product['ID'] ?>" class="btn btn-sm btn-outline-primary">Edit</a>
+                <form method="post" action="delete.php" style="display: inline-block">
+                    <input  type="hidden" name="id" value="<?php echo $product['ID'] ?>"/>
+                    <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                </form>
+                </td>
+            </tr>
+        <?php }?>
+   
+  </tbody>
+</table>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
